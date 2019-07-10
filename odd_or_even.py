a2a1fd74accb68ec8760ac40d9f307a43d98ecf0
silb90 <31510932+silb90@users.noreply.github.com>
2019-07-10 19:45:55 +0200

    add  more text in return statements

--- odd_or_even.py
+++ odd_or_even.py
@@ -14,12 +14,12 @@ def odd_or_even(bet, guess):
 
     if sum%2 == 0 and guess == "Even":
         current_balance += bet
-        return "You win! The sum is " + str(sum)
+        return "You win! The sum is " + str(sum) + " and you bet even."
     elif sum%2 == 0 and guess == "Odd":
         current_balance -= bet
-        return "You lose! The sum is " + str(sum)
+        return "You lose! The sum is " + str(sum) + " and you bet odd."
     elif sum%2 != 0 and guess == "Even":
-        return "You lose! The sum is " + str(sum)
+        return "You lose! The sum is " + str(sum) + " and you bet even."
     elif sum%2 != 0 and guess == "Odd":
-        return "You win! The sum is " + str(sum)
+        return "You win! The sum is " + str(sum) + " and you bet odd."
 
