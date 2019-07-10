4c01203fdd41890e484e4c5cc1c7faca7ddea767
silb90 <31510932+silb90@users.noreply.github.com>
2019-07-10 18:03:31 +0200

    fix increment/decrement balance

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import random
 
 money = 100
+current_balance = money
 
 def heads_or_tails(bet, guess):
+    global current_balance
     flip = random.choice([1, 2])
     print(flip)
     if guess == 1 and flip == 1:
-        current_balance = money + bet
+        current_balance += bet
         return "Heads. You win! Your current balance is " + str(current_balance)
+
     elif guess == 1 and flip == 2:
-        current_balance = money - bet
+        current_balance -= bet
         return "Heads. You lose! Your current balance is " + str(current_balance)
+
     elif guess == 2 and flip == 2:
-        current_balance = money + bet
-        return "Tales. You win! Your current balance is " + str(current_balance)
+        current_balance += bet
+        return "Tails. You win! Your current balance is " + str(current_balance)
+
     else:
-        current_balance = money - bet
-        return "Tales. You lose! Your current balance is " + str(current_balance)
+        current_balance -= bet
+        return "Tails. You lose! Your current balance is " + str(current_balance)
 
 
 result = heads_or_tails(30, 2)
